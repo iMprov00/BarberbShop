118f491bac724c36f4788640180649cc32d0e315
iMprov <improv00@mail.ru>
2025-04-10 11:29:38 +0700

    Форма записи клиента отправляет данные в user.txt

--- barbershop.rb
+++ barbershop.rb
@@ -16,3 +16,24 @@ get '/booking' do
   erb :booking
 
 end
+
+post '/booking' do
+
+  @user_name = params[:user_name]
+  @user_phone = params[:user_phone]
+  @date_zap = params[:date_zap]
+
+  @message = "Jopa"
+
+  if @user_name == "" || @user_phone == "" || @date_zap == ""
+    @message = "Не все поля заполненны!"
+  else
+    @message = "#{@user_name}, вы успешно записаны на #{@date_zap}!"
+    f = File.open "public/user.txt", "a"
+    f.write "#{@user_name}, phone: #{@user_phone}, date: #{@date_zap}\n"
+    f.close
+  end
+
+  erb :booking
+
+end
